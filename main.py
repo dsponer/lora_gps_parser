a99faac9875c237f6f74e2d5379158269732f0aa
dsponer <dsponer157@gmail.com>
2020-05-28 12:40:08 +1000

    Add def for parse coordinate

--- main.py
+++ main.py
@@ -14,30 +14,26 @@ class MapParams:
     def ll(self):
         return str(self.lon) + "," + str(self.lat)
 
-    def change_coordinate(self, x, y):
-        self.lat += x
-        self.lon += y
+    # Создание карты с соответствующими параметрами.
+    def load_map(self):
+        map_request = "http://static-maps.yandex.ru/1.x/?ll={ll}&z={z}&l={type}".format(ll=self.ll(), z=self.zoom, type=self.type)
+        response = requests.get(map_request)
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
 
+        # Запись полученного изображения в файл.
+        map_file = "map.png"
+        try:
+            with open(map_file, "wb") as file:
+                file.write(response.content)
+        except IOError as ex:
+            print("Ошибка записи временного файла:", ex)
+            sys.exit(2)
+        return map_file
 
-# Создание карты с соответствующими параметрами.
-def load_map(mp):
-    map_request = "http://static-maps.yandex.ru/1.x/?ll={ll}&z={z}&l={type}".format(ll=mp.ll(), z=mp.zoom, type=mp.type)
-    response = requests.get(map_request)
-    if not response:
-        print("Ошибка выполнения запроса:")
-        print(map_request)
-        print("Http статус:", response.status_code, "(", response.reason, ")")
-        sys.exit(1)
-
-    # Запись полученного изображения в файл.
-    map_file = "map.png"
-    try:
-        with open(map_file, "wb") as file:
-            file.write(response.content)
-    except IOError as ex:
-        print("Ошибка записи временного файла:", ex)
-        sys.exit(2)
-    return map_file
 
 def parse_coordinate(center, size_im, coordinate):
     h, w = size_im[:2]
@@ -45,7 +41,6 @@ def parse_coordinate(center, size_im, coordinate):
     x, y = coordinate
 
     return int(y * (w // 2) / center_y), int(x * (h // 2) / center_x)
-
 
 
 def main():
@@ -61,7 +56,7 @@ def main():
                 print(data)
 
         # Создаем файл
-        map_file = load_map(mp)
+        map_file = mp.load_map()
         im = cv2.imread(map_file)
         size_im = im.shape
 
@@ -70,7 +65,6 @@ def main():
 
         cv2.circle(im, (size_im[1] // 2, size_im[0] // 2), 1, (0, 0, 255), -1)
         cv2.circle(im, coordinate, 3, (0, 255, 0), -1)
-
 
         # Рисуем картинку, загружаемую из только что созданного файла.
         cv2.imshow('test', im)
